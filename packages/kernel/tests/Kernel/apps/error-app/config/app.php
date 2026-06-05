8f82cea3c39c6733ae42e6c93d69cbc1dedbc472
Daniel Ziegler <daziegler@users.noreply.github.com>
2026-06-05 11:28:19 +0200

    Wire error handling into the kernel and demo app

--- packages/kernel/tests/Kernel/apps/error-app/config/app.php
+++ packages/kernel/tests/Kernel/apps/error-app/config/app.php
@@ -3,21 +3,14 @@
 declare(strict_types=1);
 
 use Psr\Log\LoggerInterface;
-use Vestige\Environment;
-use Vestige\Http\Error\DevErrorProvider;
 use Vestige\Http\Error\ErrorHandlerMiddleware;
 use Vestige\Http\Error\ProdErrorProvider;
 use Vestige\Log\LoggingProvider;
 
-$env = Environment::tryFrom($_ENV['APP_ENV'] ?? '') ?? Environment::Production;
-
 return [
     'providers' => [
         LoggingProvider::class,
-        match ($env) {
-            Environment::Development => DevErrorProvider::class,
-            default => ProdErrorProvider::class,
-        },
+        ProdErrorProvider::class,
     ],
     'eager' => [
         LoggerInterface::class,
