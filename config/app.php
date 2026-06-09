b441668bd444c905f2e7e23b0b99f632e44153d1
Daniel Ziegler <daziegler@users.noreply.github.com>
2026-06-09 21:34:12 +0200

    Wire session subsystem into demo app

--- config/app.php
+++ config/app.php
@@ -8,12 +8,15 @@ use Vestige\Http\Error\DevErrorProvider;
 use Vestige\Http\Error\ErrorHandlerMiddleware;
 use Vestige\Http\Error\ProdErrorProvider;
 use Vestige\Log\LoggingProvider;
+use Vestige\Session\SessionMiddleware;
+use Vestige\Session\SessionProvider;
 
 $env = Environment::tryFrom($_ENV['APP_ENV'] ?? '') ?? Environment::Production;
 
 return [
     'providers' => [
         LoggingProvider::class,
+        SessionProvider::class,
         match ($env) {
             Environment::Development => DevErrorProvider::class,
             default => ProdErrorProvider::class,
@@ -24,5 +27,6 @@ return [
     ],
     'middleware' => [
         ErrorHandlerMiddleware::class,
+        SessionMiddleware::class,
     ],
 ];
